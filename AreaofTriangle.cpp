9eb4768f38f5efc889834405f9c4c54647d27fe8
Dan M <119198185+danz-km@users.noreply.github.com>
2023-04-24 22:15:14 +0800

    Create AreaofTriangle.cpp

---
+++ AreaofTriangle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void area() 
+{
+    int b=7,h=5;
+   
+    float area;
+	
+    //Write a expression to find Area as float using typecasting
+    area=(float)b*h/2;
+    
+ 
+ cout<<area;
+}
