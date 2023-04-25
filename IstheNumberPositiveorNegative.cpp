af5259ff3724e4097f7304a396beb0c5b5861251
Dan M <119198185+danz-km@users.noreply.github.com>
2023-04-25 12:56:54 +0800

    Create IstheNumberPositiveorNegative.cpp

---
+++ IstheNumberPositiveorNegative.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+void Sign(int n)
+{
+    
+    //write conditional statement here to print "positive" or "negative"
+    //well negative is anything but zeroes and positives numbers
+    if (n>=0)
+        cout<<"positive";
+    else
+        cout<<"negative";
+    
+}
