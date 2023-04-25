00ed8f330bb84f0dabba4d4c10d551d17f613142
Dan M <119198185+danz-km@users.noreply.github.com>
2023-04-25 12:54:01 +0800

    Create MaximumofTwoNumbers.cpp

---
+++ MaximumofTwoNumbers.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+void max(int x,int y)
+{
+    //write conditions here to print maximum of 2 numbers
+    //simple but the program's purpose is to print out which variable has a larger value than the other 
+    if (x>y)
+        cout<<x;
+    else
+        cout<<y;
+    
+}
